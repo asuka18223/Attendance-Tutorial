f116ab618b1f40f0edcd19fd761b5d26ed1789ae
asuka18223 <zmsnnaggddnoa2039@gmail.com>
2022-06-08 14:32:39 +0000

    追加機能１０

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,9 +14,13 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  def format_hour(time)
+    format("%.d", ((time.hour)))
+  end
 
   
   def format_min(time)
-    format("%.2d",((time.strftime('%').to_i / 15).round) * 15)
+    format("%.2d", (((time.min) / 15;) * 15))
   end
 end
